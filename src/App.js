ff85a39e474ede19fe5fedba1d2d2d15a2e7f5ea
Luciana Santos <luciydss@hotmail.com>
2022-05-11 09:46:46 -0300

    Configurações iniciais
    Reset básico de css;
    Componentes Header e Footer iniciados e Rotas definidas para Login

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import './App.css';
+import Header from './Components/Header';
+import Footer from './Components/Footer';
+import Home from './Components/Home';
+import Login from './Components/Login/Login';
+
 function App() {
-  return <div>App React</div>;
+  return (
+    <>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
