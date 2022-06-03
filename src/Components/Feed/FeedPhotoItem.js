91a0fe3fc7027a1f4c336e2f11aad91543c12754
Luciana Santos <luciydss@hotmail.com>
2022-06-03 16:55:53 -0300

    funcionalidade de deletar imagem finalizada
    overlay nas imagens ao carregar finalizado também

--- src/Components/Feed/FeedPhotoItem.js
+++ src/Components/Feed/FeedPhotoItem.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Image from '../help/Image';
 import styles from './FeedPhotosItem.module.css';
 
 const FeedPhotoItem = ({ photo, setModalPhoto }) => {
@@ -8,7 +9,8 @@ const FeedPhotoItem = ({ photo, setModalPhoto }) => {
 
   return (
     <li className={styles.photo} onClick={handleClick}>
-      <img src={photo.src} alt={photo.title} />
+      <Image src={photo.src} alt={photo.title} />
+
       <span className={styles.views}>{photo.acessos}</span>
     </li>
   );
--- src/Components/Photo/PhotoContent.js
+++ src/Components/Photo/PhotoContent.js
@@ -1,19 +1,28 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import PhotoComments from './PhotoComments';
 import styles from './PhotoContent.module.css';
+import { UserContext } from '../../UserContext';
+import PhotoDelete from './PhotoDelete';
+import Image from '../help/Image';
 
 const PhotoContent = ({ data }) => {
+  const user = useContext(UserContext);
   const { photo, comments } = data;
 
   return (
     <div className={styles.photo}>
       <div className={styles.img}>
-        <img src={photo.src} alt={photo.title} />
+        <Image src={photo.src} alt={photo.title} />
       </div>
       <div className={styles.details}>
         <p className={styles.author}>
-          <Link to={`/perfil/${photo.author}`}>@{photo.author}</Link>
+          {user.data && user.data.username === photo.author ? (
+            <PhotoDelete id={photo.id} />
+          ) : (
+            <Link to={`/perfil/${photo.author}`}>@{photo.author}</Link>
+          )}
+
           <span className={styles.views}>{photo.acessos}</span>
         </p>
         <h1 className="title">
