0cd051d56df642939ef554fdb66b75ad532b9eff
Luciana Santos <luciydss@hotmail.com>
2022-06-06 22:55:42 -0300

    página 404 finalizada

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ import { UserStorage } from './UserContext';
 import ProtectedRoute from './Components/help/ProtectedRoute';
 import User from './Components/User/User';
 import Photo from './Components/Photo/Photo';
+import UserProfile from './Components/User/UserProfile';
+import NotFound from './Components/NotFound';
 
 function App() {
   return (
@@ -27,6 +29,8 @@ function App() {
               }
             />
             <Route path="foto/:id" element={<Photo />} />
+            <Route path="perfil/:user" element={<UserProfile />} />
+            <Route path="*" element={<NotFound />} />
           </Routes>
           <Footer />
         </UserStorage>
